feabb5f71805c5895fa9bab568fc71084b1839ca
student <student@pyclassvm>
2016-08-06 13:13:09 -0400

    Env Sustain intial website sample

---
+++ README.md
@@ -0,0 +1,5 @@
+#Environmental Sustainability Project
+
+This website is a product of a JHU class to demonstrate using open data with Python.  The website displays a research proposal and methodology using skills gained during the class.
+
+The goal is to understand if increased recognition of the importance of environmental sustainability measures are resulting in improvement of environmental health as a result of policy or practice implementations.
---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+@app.route('/research')
+def research():
+	return render_template('research.html')
+
+@app.route('/results')
+def results():
+	return render_template('results.html')
+
+@app.route('/methodology')
+def methodology():
+	return render_template('methodology.html')
+
+if __name__ =='__main__':
+	app.run(debug=True)
